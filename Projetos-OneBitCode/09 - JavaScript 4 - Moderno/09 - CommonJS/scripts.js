d63b964a83489802fd0e6e5fab633e4fe4e7e559
AndyTrindade25 <anderson.trindade00@gmail.com>
2023-01-09 15:41:59 -0300

    Atualizacao

---
+++ Projetos-OneBitCode/09 - JavaScript 4 - Moderno/09 - CommonJS/scripts.js
@@ -0,0 +1,19 @@
+function render() {
+  console.log("Renderizando a interface da aplicação...");
+}
+
+function store() {
+  console.log("Salvando as informações no banco de dados...");
+}
+
+console.log("Aplicação iniciada.");
+render();
+store();
+console.log("Aplicação finalizada.");
+// render.js
+
+function render() {
+  console.log("Renderizando a interface da aplicação...");
+}
+
+module.exports = render;
