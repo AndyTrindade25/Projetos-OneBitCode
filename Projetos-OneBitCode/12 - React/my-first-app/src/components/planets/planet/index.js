f38735110baca48fceb544e7baae5b17a54a31b0
Anderson Trindade <anderson.trindade00@gmail.com>
2023-02-05 09:40:24 -0300

    Atualizacao do curso com React

---
+++ Projetos-OneBitCode/12 - React/my-first-app/src/components/planets/planet/index.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react'
+
+const Planet = () => {
+    return (
+        <Fragment>
+            <h4>Mercúrio</h4>
+            <p>Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...Lorem Ipsum...</p>
+        </Fragment>
+    )
+}
+
+export default Planet;
