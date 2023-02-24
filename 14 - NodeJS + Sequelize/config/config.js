867e62fe81d02f4479fca8cfadbae52baa6a62a7
anderson.trindade00 <anderson.trindade00@gmail.com>
2023-02-24 17:09:42 -0300

    Atualização do curso

---
+++ 14 - NodeJS + Sequelize/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "anderson",
+    password: "anderson123",
+    database: "curso_sequelize",
+    define: {
+      timestamps: true,
+    },
+  };
